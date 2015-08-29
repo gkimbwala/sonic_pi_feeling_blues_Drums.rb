936d5a3339895be902aa7c5ee4aac94908705ad3
gkimbwala <gkimbwala@gmail.com>
2015-08-29 09:10:25 -0700

    added notes
    added notes to drumbeats

--- drums.rb
+++ drums.rb
@@ -14,87 +14,95 @@ trip_uh = 0.6666
 let = 0.3333
 
 chord_progression = [
-  one, four, one, one,
-  four, four, one, one,
-  five, four, one, one
+one, four, one, one,
+four, four, one, one,
+five, four, one, one
 ]
 
 in_thread(name: :bass) do
-  chord_progression.each do |note|
-    bass_line note
-  end
+    chord_progression.each do |note|
+        bass_line note
+    end
 end
 
 in_thread(name: :comp) do
-  chord_progression.each do |note|
-    comp_line note
-  end
+    chord_progression.each do |note|
+        comp_line note
+    end
 end
 
 define :bass_line do |root|
-  cue :tick
-  [root, root + 9, root + 10, root + 9].each do |note|
-    play note, release: trip_uh
-    sleep trip_uh
-    play root + 7, amp: 0.5, release: let, amp: 2
-    sleep let
-  end
+    cue :tick
+    [root, root + 9, root + 10, root + 9].each do |note|
+        play note, release: trip_uh
+        sleep trip_uh
+        play root + 7, amp: 0.5, release: let, amp: 2
+        sleep let
+    end
 end
 
 define :comp_line do |root|
-  sync :tick
-  root_8va = root + 12
-  play chord(root_8va, :dom7), release: trip_uh, amp: 12
-  sleep trip_uh
-  play chord(root_8va, :dom7), release: let, amp: 8
-  sleep let + 3
+    sync :tick
+    root_8va = root + 12
+    play chord(root_8va, :dom7), release: trip_uh, amp: 12
+    sleep trip_uh
+    play chord(root_8va, :dom7), release: let, amp: 8
+    sleep let + 3
 end
 
 
 # Drum loop three, quarter notes
 live_loop :drums do
-  sync :tick
- beat_length = 0.5
-
-  in_thread(name: :drumPedal) do
-    16.times do
-      sample :drum_cymbal_pedal, amp: 2
-      sleep beat_length
-    end
-  end
-
-  in_thread(name: :drumSplash) do
-    4.times do
-      sample :drum_splash_soft, amp: 2
-      sleep 4
-    end
-  end
-
-  in_thread(name: :drumHiHat) do
-   4.times do
-      sample :drum_cymbal_closed, amp: 1
-      sleep beat_length
-    end
-  end
+    sync :tick
+    beat_length = 0.5
     
-  in_thread(name: :bassDrum) do
-  3.times do
-      sample :drum_bass_hard, amp: 2
-      sleep 2
+    # add drum pedal to keep time
+    in_thread(name: :drumPedal) do
+        16.times do
+            sample :drum_cymbal_pedal, amp: 2
+            sleep beat_length
+        end
     end
-  2.times do
-      sample :drum_bass_hard, amp: 2
-      sleep beat_length
+    
+    #add a drum splash on the one of the measure
+    in_thread(name: :drumSplash) do
+        4.times do
+            sample :drum_splash_soft, amp: 2
+            sleep 4
+        end
     end
-  sleep 0.5
-  end
-
-  in_thread(name: :snareDrum) do
-  2.times do
-      sleep 1
-      sample :drum_snare_soft, amp: 3
-      sleep 1
+    
+    #add a hi_hat
+    in_thread(name: :drumHiHat) do
+        4.times do
+            sample :drum_cymbal_closed, amp: 1
+            sleep beat_length
+        end
     end
-  end
-sleep 1
+    
+    #add a drum beat
+    in_thread(name: :bassDrum) do
+        #3 measures of traditional blues beat
+        3.times do
+            sample :drum_bass_hard, amp: 2
+            sleep 2
+        end
+        
+        # 1 meausre of bass off beat
+        2.times do
+            sample :drum_bass_hard, amp: 2
+            sleep beat_length
+        end
+        sleep 0.5
+    end
+    
+    #snare drum on the 2 and 4
+    in_thread(name: :snareDrum) do
+        2.times do
+            sleep 1
+            sample :drum_snare_soft, amp: 3
+            sleep 1
+        end
+    end
+    sleep 1
 end
